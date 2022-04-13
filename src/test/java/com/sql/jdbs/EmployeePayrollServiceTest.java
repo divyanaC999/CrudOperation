3bd3868ac733a244e73650f1700c9e8388be42e3
divyanaC999 <divyachourasiya999@gmail.com>
2022-04-13 16:09:41 +0530

    Updated: EmployeePayroll

---
+++ src/test/java/com/sql/jdbs/EmployeePayrollServiceTest.java
@@ -0,0 +1,17 @@
+
+package com.sql.jdbs;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EmployeePayrollServiceTest {
+	@Test
+	public void givenEmployeePayrollInDB_WhenRetrived_ShouldMatchEmployeeCount() {
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(DB_IO);
+		Assert.assertEquals(3, employeePayrollData.size());
+	}
+
+}
